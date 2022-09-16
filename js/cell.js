f73855aed455233bcb28df39290cc4e217203624
sandy0903 <duyenhuynh0903@gmail.com>
2022-09-16 20:10:49 +0700

    delete files

--- js/cell.js
+++ js/cell.js
@@ -7,8 +7,32 @@ function Cell(x=0,y=0,piece,color='gray'){
     this.renderCell=function(){
         this.cell.style.backgroundColor=this.color
         this.cell.className="square"
+        this.cell.addEventListener("click", (event) => this.checkValidMove(event));
         return this.cell
     }
+    this.checkValidMove=function(event){
+    switch (this.piece.name) {
+      case "pawn":
+        this.piece.checkPawnMove(event)
+        break;
+      case "bishop":
+        this.piece.checkBishopMove(event)
+         break;
+      case "king":
+        this.piece.checkKingMove(event)
+         break;
+      case "knight":
+        this.piece.checkKnightMove(event)
+         break;
+      case "rook":
+        this.piece.checkRookMove(event)
+         break;
+      case "queen":
+        this.piece.checkQueenMove(event)
+         break;
+    }
+
+  }
 
 
 }
--- js/pieces/pawn.js
+++ js/pieces/pawn.js
@@ -5,10 +5,19 @@ function Pawn(x,y,isWhite, isKilled = false) {
   Piece.call(this, isWhite, isKilled, PieceName.PAWN, pawnImgUrl)
   Board.call(this)
   this.checkPawnMove=function(event){
-      const cellOfPawn= event.target.parentElement
-      this.initialize()
-      console.log(this.rows)
+      const board=this.initialize()
+      if(this.x==1 || this.x==6){
+        board[this.x-1].cells[this.y].color="pink"
+        board[this.x-2].cells[this.y].color="pink"
+      }else{
+        board[this.x-1].cells[this.y].color="pink"
+      }
+      this.renderBoard()
+      console.log(board[this.x-1].cells[this.y])
 
     }
   }
 
+// test case cho pawn
+// + check firsrMove ? go 2 cell : go 1
+// + check tai vi tri board[x-1].cells[y+1] && cells[y-1] if yes=> exist piece ?  can change y position of pawn : go 1 cell
