9721fe03802d00863275285de23820b016dbe136
Hoang Quan <nguyen.quan.it.99@gmail.com>
2022-09-19 22:43:28 +0700

    create uml

--- js/pieces/bishop.js
+++ js/pieces/bishop.js
@@ -1,7 +1,6 @@
 function Bishop(isWhite, isKilled = false) {
-    const bigshopImgUrl = isWhite ? '../chess-img/bishop_white.png' : '../chess-img/bishop_black.png'
-
-    Piece.call(this, isWhite, isKilled, PieceName.BISHOP, bigshopImgUrl)
+    const bigshopImgUrl = isWhite ? '../chess-img/bishop_white.png' : '../chess-img/bishop_black.png';
+    Piece.call(this, isWhite, isKilled, PieceName.BISHOP, bigshopImgUrl);
     this.checkBishopMove=function(){
       console.log("piece")
     }
--- js/pieces/pawn.js
+++ js/pieces/pawn.js
@@ -1,6 +1,6 @@
 function Pawn(x,y,isWhite, isKilled = false) {
   this.x=x;
-  this.y=y
+  this.y=y;
   const pawnImgUrl = isWhite ? '../chess-img/pawn_white.png' : '../chess-img/pawn_black.png'
   Piece.call(this, isWhite, isKilled, PieceName.PAWN, pawnImgUrl)
   Board.call(this)
